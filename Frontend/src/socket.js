00e645e711aa87bc0d354e39c613e936454ae451
ThanHoang1771 <batman120709@gmail.com>
2026-08-31 17:30:30 +0700

    Update socket.js

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -1,3 +1,3 @@
-import { io } from "socket.io-client";
-const socket = io("http://localhost:4000");
-export default socket;
+import { io } from 'socket.io-client';
+// Đổi URL này sang link server render/vercel khi đưa lên host thật
+export const socket = io('https://banpicktest.onrender.com/');
